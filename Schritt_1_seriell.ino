053395e752e2e768f816f9657d1cf0bed54100cb
Marcus <you@example.com>
2021-09-01 19:33:23 +0200

    Endversion mit beiden Achsen

--- Schritt_1_seriell.ino
+++ Schritt_1_seriell.ino
@@ -1,59 +1,83 @@
 
 #include <Stepper.h>
 
-
-int calculateSteps(int speed);
-
 const int StepsPerRevolution = 2038;
-int stepCounter = 0;
-int maxSteps = 300;
-Stepper myStepper(StepsPerRevolution, 8, 10, 9, 11);
-
-int myTeiler = 1024/24;
+int xPos;
+int yPos;
+int maxStepsX = 2038;
+int maxStepsY = 2038;
+Stepper xStepper(StepsPerRevolution, 8, 10, 9, 11);
+Stepper yStepper(StepsPerRevolution, 4, 6, 5, 7);
 
 int xInput = A0;
 int yInput = A1;
-int Switch = 7;
-
+int xSwitchPIN = 2;
+int ySwitchPIN = 2;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(Switch, INPUT_PULLUP);
-  stepCounter = 0; 
+  pinMode(xSwitchPIN, INPUT_PULLUP);
+
+  searchZeroPos(xStepper, xSwitchPIN);
+  xPos = 0;
+  delay(1000);
+  searchZeroPos(yStepper, ySwitchPIN);
+  yPos = 0;
+
+  Serial.println("Ursprung gesetzt.");
+  Serial.println("(0;0)");
 }
 
 void loop() {
   
   int xValue = analogRead(xInput);
-  int xValuehalbe = (xValue - 512);
-  int mySpeed = map(xValue, 0, 1023, -12, 12);
-
-  Serial.print("speed: ");
-  Serial.println(mySpeed);
-  if (mySpeed != 0) {
-    myStepper.setSpeed(abs(mySpeed));
+  int xSteps = calculateSteps(map(xValue, 0, 1023, 12, -12),maxStepsX, xPos);
+   
+  if (xSteps != 0) {
+    xStepper.setSpeed(abs(xSteps/2));
   }
   
-  int steps = calculateSteps(mySpeed);
-  myStepper.step(steps);
+  xStepper.step(xSteps);
+  xPos += xSteps;
   
-  Serial.print("steps: ");
-  Serial.println(steps);
-  stepCounter += steps;
+  int yValue = analogRead(yInput);
+  int ySteps = calculateSteps(map(yValue, 0, 1023, 12, -12),maxStepsY, yPos);
   
-  Serial.print("position: ");
-  Serial.println(stepCounter);     
+   if (ySteps != 0) {
+    yStepper.setSpeed(abs(ySteps/2));
+  }
+  
+  yStepper.step(ySteps);
+  yPos += ySteps;
+  
+  if ((xSteps != 0)||(ySteps != 0)){
+    Serial.print("(");
+    Serial.print(xPos);   
+    Serial.print(";");
+    Serial.print(yPos);
+    Serial.println(")");
+  }
 }
 
-int calculateSteps(int speed) {
+int calculateSteps(int value, int maxSteps, int pos) {
   int steps = 0;
-  if (speed < 0) {
-    steps = -min(stepCounter, -2*speed);
+  if (value < 0) {
+    steps = -min(pos, -2*value);
   } else {
-    steps = 2*speed;
-    if (stepCounter + steps > maxSteps) {
-      steps = maxSteps - stepCounter;
+    steps = 2*value;
+    if (pos + steps > maxSteps) {
+      steps = maxSteps - pos;
     }
   }
   return steps;
+}
+
+int searchZeroPos(Stepper stepper, int switchPin) {
+  int switchPressed = digitalRead(switchPin);
+  
+   while (switchPressed == 1) {
+    stepper.setSpeed(12);
+    stepper.step(24);
+    switchPressed = digitalRead(switchPin);
+  }
 }
